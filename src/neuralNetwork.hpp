d3d9e6e421d0b70766d643ea6264010aafc1a44c
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2025-07-22 21:25:58 +0200

    Add files via upload

--- src/neuralNetwork.hpp
+++ src/neuralNetwork.hpp
@@ -2,7 +2,7 @@
 
     neuralNetwork.hpp
 
-    This file is part of Clap Recognition Using a Neural Network from Scratch (C++ for Arduino): https://github.com/BojanJurca/Clap-Recognition-Using-a-Neural-Network-from-Scratch-Cpp-for-Arduino
+    This file is part of Clap Recognition Using a Neural Network from Scratch (C++ for Arduino): https://
 
     The neural network is implemented as a C++ variadic template. This approach was chosen to simplify both the usage 
     of the library and the modification of the network topology, making it as intuitive and flexible as possible.
@@ -16,8 +16,8 @@
         //                   |    |    |                               |      |
         neuralNetworkLayer_t<13, ReLU, 16, / * add more if needed * / Sigmoid, 2> neuralNetwork;
 
-    
-    Bojan Jurca, May 22, 2025
+
+    Bojan Jurca, Aug 12, 2025
 
 */
 
@@ -31,58 +31,61 @@
 
     // non-linear neuron activation function and its derivative
 
-        static float __Sigmoid__ (float x) { return 1.f / (1.f + expf (-x)); }
-        static float __SigmoidDerivative__ (float x) { float s = __Sigmoid__ (x); return s * (1 - s); }
-        
-        static float __ReLU__ (float x) { return x <= 0.f ? 0.f : x; }
-        static float __ReLUDerivative__ (float x) { return x <= 0.f ? 0.f : 1.f; }
-
         #define Sigmoid 0
         #define ReLU 1
 
-        float (*__af__ []) (float) = {__Sigmoid__, __ReLU__};
-        float (*__af_derivative__ []) (float) = {__SigmoidDerivative__, __ReLUDerivative__};
+        // neuron activation function (Sigmoid or ReLU)
+        template <size_t activationFunction>
+        float af (float x) {
+            if constexpr (activationFunction == Sigmoid) {
+                return 1.f / (1.f + exp (-x)); // Sigmoid
+            } else if constexpr (activationFunction == ReLU) {
+                return x <= 0.f ? 0.f : x; // ReLU
+            } else {
+                static_assert (activationFunction < 2, "Unsupported activation function");
+                return 0.f;
+            }
+        }
+        
+        // neuron activation function derivative (Sigmoid' or ReLU')
+        template <size_t activationFunction>
+        float af_derivative (float x) {
+            if constexpr (activationFunction == Sigmoid) {
+                float s = af<Sigmoid> (x);
+                return s * (1 - s); // Sigmoid'
+            } else if constexpr (activationFunction == ReLU) {
+                return x <= 0.f ? 0.f : 1.f; // ReLU'
+            } else {
+                static_assert (activationFunction < 2, "Unsupported activation function");
+                return 0.f;
+            }
+        }
 
+        // random initializer
+        float randomInitializer (size_t inputCount, size_t activationFunction, size_t neuronCount) {
+            // use Box-Muller Transform to calculate normaly distributed random variable from uniformly distrubuted random function
+        
+                // select 2 independent uniformly distributed random values in interval (0, 1)
+                float U1 = ((float) (rand () % (RAND_MAX - 1) + 1)) / RAND_MAX;
+                float U2 = ((float) (rand () % (RAND_MAX - 1) + 1)) / RAND_MAX;
+                
+                // use Box-Muller to tranform them to two independed normally distributed random values with mean of 0 and variance of 1
+                float N1 = sqrt (-2 * log (U1)) * cos (2 * M_PI * U2); 
+                // float N2 = sqrt (-2 * log (U1)) * sin (2 * M_PI * U2); // we don't actually need the second independent random variable here
+        
+            // apply the desired mean and variance
+                if (activationFunction == Sigmoid) {
+                    return 0 + sqrt (2.0 / (inputCount + neuronCount)) * N1; // Xavier
+                } else if (activationFunction == ReLU) {
+                    return 0 + sqrt (2.0 / inputCount) * N1; // He
+                } else {
+                    // static_assert (activationFunction < 2, "Unsupported activation function");
+                    return 0.f;
+                }
+        }
 
     // training
 
-
-        // initialization function Xavier returns normally distributed random number with a mean of 0 and a standard deviation of sqrt (2/ (inputCount + neuronCount)
-        // it is usually used with Sigmoid activation function
-        float __Xavier__ (size_t inputCount, size_t neuronCount) {
-            // let's use Box-Muller Transform to calculate normaly distributed random variable from uniformly distrubuted random function
-
-                // 1. select 2 independent uniformly distributed random values in interval (0, 1)
-                float U1 = ((float) (rand () % (RAND_MAX - 1) + 1)) / RAND_MAX;
-                float U2 = ((float) (rand () % (RAND_MAX - 1) + 1)) / RAND_MAX;
-                
-                // 2. use Box-Muller transtor to tranform them to two independed normally distributed random values with mean of 0 and variance of 1
-                // float N1 = sqrt (-2 * log (U1)) * cos (2 * M_PI * U2); 
-                float N2 = sqrt (-2 * log (U1)) * sin (2 * M_PI * U2); // we don't actually need the second independent random variable here
-
-            // apply the desired mean of 0 and variance of sqrt (2.0 / (inputCount + neuronCount)) to random variable N1
-                return 0 + sqrt (2.0 / (inputCount + neuronCount)) * N2; 
-        }
-        
-        // initialization function He returns normally distributed random number with a mean of 0 and a standard deviation of sqrt (2/inputCountn)
-        // it is usually used with ReLU activation function
-        float __He__ (size_t inputCount, size_t neuronCount) {
-            // let's use Box-Muller Transform to calculate normaly distributed random variable from uniformly distrubuted random function
-
-                // 1. select 2 independent uniformly distributed random values in interval (0, 1)
-                float U1 = ((float) (rand () % (RAND_MAX - 1) + 1)) / RAND_MAX;
-                float U2 = ((float) (rand () % (RAND_MAX - 1) + 1)) / RAND_MAX;
-                
-                // 2. use Box-Muller transtor to tranform them to two independed normally distributed random values with mean of 0 and variance of 1
-                float N1 = sqrt (-2 * log (U1)) * cos (2 * M_PI * U2); 
-                // float N2 = sqrt (-2 * log (U1)) * sin (2 * M_PI * U2); // we don't actually need the second independent random variable here
-
-            // apply the desired mean of 0 and variance of sqrt (2.0 / inputCount) to random variable N1
-                return 0 + sqrt (2.0 / inputCount) * N1; 
-        }
-        
-        float (*__randomInitializer__ []) (size_t, size_t) = {__Xavier__, __He__};        
-        
         // learning rate
         #define learningRate 0.01
 
@@ -93,8 +96,8 @@
 
 
     // hidden layers
-        template <size_t inputCount, size_t af, size_t neuronCount, size_t... sizes> 
-        class neuralNetworkLayer_t<inputCount, af, neuronCount, sizes...> {
+        template <size_t inputCount, size_t activationFunction, size_t neuronCount, size_t... sizes> 
+        class neuralNetworkLayer_t<inputCount, activationFunction, neuronCount, sizes...> {
 
                 // data structures needed for this layer: weight and bias
                 float weight [neuronCount][inputCount];
@@ -102,7 +105,6 @@
 
                 // include the next layer instance which will include the next layer itself, ...
                 neuralNetworkLayer_t<neuronCount, sizes...> nextLayer;
-
 
             public:
             
@@ -119,7 +121,7 @@
                             neuron [n] = bias [n];
                             for (size_t i = 0; i < inputCount; i++)
                                 neuron [n] += weight [n][i] * input [i];
-                            neuron [n] = __af__ [af] (neuron [n]);
+                            neuron [n] = af<activationFunction> (neuron [n]);
                         }
 
                     // return what the next layer thinks about the neurons clculated here
@@ -134,7 +136,7 @@
                     for (size_t n = 0; n < neuronCount; n++) {
                         for (size_t i = 0; i < inputCount; i++)
                             // weight are typically initialized with random numbers, He function is is particularly suitable to be used with ReLU activation function
-                            weight [n][i] = __randomInitializer__ [af] (inputCount, neuronCount);
+                            weight [n][i] = randomInitializer (inputCount, activationFunction, neuronCount);
                         // bias are typically set to 0 at initialization
                         bias [n] = 0;
                     }
@@ -155,7 +157,7 @@
                             z [n] = bias [n];
                             for (size_t i = 0; i < inputCount; i++)
                                 z [n] += weight [n][i] * input [i];
-                            neuron [n] = __af__ [af] (z [n]);
+                            neuron [n] = af<activationFunction> (z [n]);
                         }
 
                     // calculate the first part of delta in the next layer then apply activation function derivative here
@@ -164,7 +166,7 @@
                         float error = nextLayer.backwardPropagation (neuron, expected, delta);
                         // calculate only the second part of delta, the first par has already been calculated at the next layer
                         for (size_t n = 0; n < neuronCount; n++)
-                            delta [n] *= __af_derivative__ [af] (z [n]);
+                            delta [n] *= af_derivative<activationFunction> (z [n]);
 
                     // update weight and bias at this layer
                         for (size_t n = 0; n < neuronCount; n++) {
@@ -190,7 +192,7 @@
                     return error;
                 }
 
-                // export the whole model as C++ int32_t initializer list
+                // export the whole model as C++ initializer list
                 friend ostream& operator << (ostream& os, const neuralNetworkLayer_t& nn) {
                     int32_t *p = (int32_t *) &nn;
                     os << "{";
@@ -205,8 +207,7 @@
                     os << "}\n";
                     return os;
                 }
-
-                // import the model form C++ initializer list or int32_t array
+                
                 template<size_t N>
                 neuralNetworkLayer_t& operator = (const int32_t (&model) [N]) {
                     static_assert (N == sizeof (*this) / sizeof (int32_t), "Wrong size of model!");
@@ -218,13 +219,13 @@
 
 
     // output layer
-        template <size_t inputCount, size_t af, size_t neuronCount> 
-        class neuralNetworkLayer_t<inputCount, af, neuronCount> {
+        template <size_t inputCount, size_t activationFunction, size_t neuronCount> 
+        class neuralNetworkLayer_t<inputCount, activationFunction, neuronCount> {
 
                 // data structures needed for this layer: weight and bias
                 float weight [neuronCount][inputCount];
                 float bias [neuronCount];
-
+                
             public:
 
                 static constexpr size_t outputCount = neuronCount;
@@ -240,7 +241,7 @@
                             neuron [n] = bias [n];
                             for (size_t i = 0; i < inputCount; i++)
                                 neuron [n] += weight [n][i] * input [i];
-                            neuron [n] = __af__ [af] (neuron [n]);
+                            neuron [n] = af<activationFunction> (neuron [n]);
                         }
 
                     // softmax normalization of the result
@@ -265,7 +266,7 @@
                     for (size_t n = 0; n < neuronCount; n++) {
                         for (size_t i = 0; i < inputCount; i++)
                             // weight are typically initialized with random numbers, He function is is particularly suitable to be used with ReLU activation function
-                            weight [n][i] = __randomInitializer__ [af] (inputCount, neuronCount);
+                            weight [n][i] = randomInitializer (inputCount, activationFunction, neuronCount);
                         // bias are typically set to 0 at initialization
                         bias [n] = 0;
                     }
@@ -286,7 +287,7 @@
                             z [n] = bias [n];
                             for (size_t i = 0; i < inputCount; i++)
                                 z [n] += weight [n][i] * input [i];
-                            neuron [n] = __af__ [af] (z [n]);
+                            neuron [n] = af<activationFunction> (z [n]);
                         }
 
                     // calculate the error
@@ -303,7 +304,7 @@
                         float delta [neuronCount]; 
                         for (size_t n = 0; n < neuronCount; n++) {
                             // calculat delta at output layer    
-                            delta [n] = (neuron [n] - expected [n]) * __af_derivative__ [af] (z [n]);
+                            delta [n] = (neuron [n] - expected [n]) * af_derivative<activationFunction> (z [n]);
 
                             // update weight
                             for (size_t i = 0; i < inputCount; i++)
@@ -327,7 +328,7 @@
                 }
 
 
-                // export the whole model as C++ int32_t initializer list
+                // export the whole model as C++ initializer list
                 friend ostream& operator << (ostream& os, const neuralNetworkLayer_t& nn) {
                     int32_t *p = (int32_t *) &nn;
                     os << "{";
@@ -342,8 +343,7 @@
                     os << "}\n";
                     return os;
                 }
-
-                // import the model form C++ initializer list or int32_t array
+                
                 template<size_t N>
                 neuralNetworkLayer_t& operator = (const int32_t (&model) [N]) {
                     static_assert (N == sizeof (*this) / sizeof (int32_t), "Wrong size of model!");
